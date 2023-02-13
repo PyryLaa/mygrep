81fa1475db872a4b3e96f53f034b27445bc57fdc
PyryL <pyry.laaksonen@tuni.fi>
2023-02-13 15:34:06 +0200

    Initial commit

---
+++ my_grep/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(){
+
+	std::string usr_in, search;
+	std::cout << "Give a string from which to search for: ";
+	std::getline(std::cin, usr_in);
+	std::cout << "Give search string: ";
+	std::getline(std::cin, search);
+}
