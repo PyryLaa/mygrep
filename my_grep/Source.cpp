674d5945b03e6f592fb4fd3244b32f0e16c3d5ed
PyryL <pyry.laaksonen@tuni.fi>
2023-02-14 09:15:41 +0200

    1st increment done, no error detection yet

--- my_grep/Source.cpp
+++ my_grep/Source.cpp
@@ -9,4 +9,14 @@ int main(){
 	std::getline(std::cin, usr_in);
 	std::cout << "Give search string: ";
 	std::getline(std::cin, search);
+
+	std::size_t found = usr_in.find(search);
+
+	if (found != std::string::npos) {
+		std::cout << '"' << search << '"' << "found in" << '"' << usr_in << '"' << "in position " << found;
+	}
+	else {
+		std::cout << '"' << search << '"' << "NOT found in " << '"' << usr_in << '"';
+	}
+	return 0;
 }
