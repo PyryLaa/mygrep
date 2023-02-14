0bcabd0cf9928609ab46b04d6ab6f0e39ca45b38
PyryL <pyry.laaksonen@tuni.fi>
2023-02-14 12:27:31 +0200

    Changed the input searching to its own function

--- my_grep/my_grep.cpp
+++ my_grep/my_grep.cpp
@@ -2,26 +2,38 @@
 #include <string>
 #include <fstream>
 
+void str_search(std::string*, std::string*);
+
 int main(){
 
 	std::string usr_in, search;
+	std::string* input_ptr, * search_ptr;
 	std::cout << "Give a string from which to search for: ";
 	std::getline(std::cin, usr_in);
 	std::cout << "Give search string: ";
 	std::getline(std::cin, search);
 
-	if (search.length() > usr_in.length()) {
+	if (search.length() > usr_in.length()) { //Test if the search string is longer than the input string
 		std::cout << "Search string too long, give new search string: ";
 		std::getline(std::cin, search);
 	}
 
-	std::size_t found = usr_in.find(search);
+	input_ptr = &usr_in;
+	search_ptr = &search;
+	str_search(input_ptr, search_ptr);
+	
+	return 0;
+}
+
+void str_search(std::string* input, std::string* searcher) { //Function for searching the string in the input string
+	std::string inputstr = *input, searchstr = *searcher;
+	std::size_t found = inputstr.find(searchstr);
 
 	if (found != std::string::npos) {
-		std::cout << '"' << search << '"' << " found in " << '"' << usr_in << '"' << " in position " << found;
+		std::cout << '"' << searchstr << '"' << " found in " << '"' << inputstr << '"' << " in position " << found;
 	}
 	else {
-		std::cout << '"' << search << '"' << " NOT found in " << '"' << usr_in << '"';
+		std::cout << '"' << searchstr << '"' << " NOT found in " << '"' << inputstr << '"';
 	}
-	return 0;
+
 }
