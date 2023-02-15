1de0fff09bb07ffcb83a4d9ca226bc11a46e6e09
PyryL <pyry.laaksonen@tuni.fi>
2023-02-15 15:13:46 +0200

    Case-sensitivity check and uppercase conversion added

--- my_grep/my_grep.cpp
+++ my_grep/my_grep.cpp
@@ -1,25 +1,39 @@
+//For now, this program will ask for a string from the user and then ask another string to search from the first given string
+
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 void str_search(std::string*, std::string*);
+std::string str_convert(std::string*, std::string*);
 
 int main(){
 
 	std::string usr_in, search;
 	std::string* input_ptr, * search_ptr;
+	char case_s;
 	std::cout << "Give a string from which to search for: ";
 	std::getline(std::cin, usr_in);
 	std::cout << "Give search string: ";
 	std::getline(std::cin, search);
 
-	if (search.length() > usr_in.length()) { //Test if the search string is longer than the input string
+	std::cout << "Do you want the search to be case-sensitive?(Y/N): ";
+	std::cin >> case_s;
+
+	while (search.length() > usr_in.length()) { //Test if the search string is longer than the input string
 		std::cout << "Search string too long, give new search string: ";
 		std::getline(std::cin, search);
 	}
 
-	input_ptr = &usr_in;
-	search_ptr = &search;
+	if (case_s == 'N' || case_s == 'n') { //Converts the input and search strings to uppercase if case-sensitivity is not chosen
+		std::transform(usr_in.begin(), usr_in.end(), usr_in.begin(), ::toupper);
+		std::transform(search.begin(), search.end(), search.begin(), ::toupper);
+	}
+
+	input_ptr = &usr_in; //Pointer to user input, will be passed to search function
+	search_ptr = &search; //Pointer to search string, will be passed to search function
+
 	str_search(input_ptr, search_ptr);
 	
 	return 0;
@@ -37,3 +51,4 @@ void str_search(std::string* input, std::string* searcher) { //Function for sear
 	}
 
 }
+
