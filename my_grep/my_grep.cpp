9908202c1ffdafbde2ac9606dd1da065bc614bf9
PyryL <pyry.laaksonen@tuni.fi>
2023-02-22 15:06:19 +0200

    Increment 3 done, added command line options

--- my_grep/my_grep.cpp
+++ my_grep/my_grep.cpp
@@ -3,25 +3,49 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <stdarg.h>
+
+
 
 void str_search(std::string*, std::string*); //Function for searching another string from a string
 void input_search();//If no additional arguments given, program goes to this function
-void file_search(std::string, std::string);//If given search string and file name, go here
+void file_search(int arg_c, ...);//If given search string and file name, go here
 
 int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "fi_FI"); //Enable the use of ä and ö
+
 	
+
+	if (argc == 2) {
+		std::string help = argv[1];
+		if (help == "-h" || help == "-?") {
+			std::cout << "Usage: If you give no arguments, program will ask you to give string and then another string to search from the first"
+				<< "\nAccepted arguments (give in this order): <search options> <search string> <filename>"
+				<< "\nAccepted file extension is .txt"
+				<< "\nAccepted search options (these are optional): <-ol> <-oo> <-olo>"
+				<< "\n<-ol> gives you the count from which line the string is found"
+				<< "\n<-oo> gives you the count of lines found"
+				<< "\n<-olo> gives you both counts";
+		}
+
+	}
+
 	if (argc == 1) {//If no other argumens than program name is given, run this
 		input_search();
 	}
-	else if (argc >= 2) {
+	else if (argc == 3) { //If only search string and file from which to search is given
 		std::string search_str = argv[1], filename = argv[2];
-		file_search(search_str, filename);
+		file_search(argc, filename, search_str);
+	}
+	else if (argc == 4) { //If other options are given for searching
+		std::string option_str = argv[1], search_str = argv[2], filename = argv[3];
+		file_search(argc, filename, search_str, option_str);
 	}
 	return 0;
 }
 
 
-void input_search() {
+void input_search() { //If no args given when starting the program, goes here. Asks user for two strings and searches the other with the other
 	std::string usr_in, search;
 	std::string* input_ptr, * search_ptr;
 	char case_s;
@@ -62,32 +86,101 @@ void str_search(std::string* input, std::string* searcher) { //Function for sear
 	}
 
 }
-void file_search(std::string srch, std::string fname) {
+void file_search(int arg_c, ...) { //Searches the given file for the given string, additional options include line count in the file and count of found lines
+	va_list argl; //This is the list of arguments
+	va_start(argl, arg_c);
+
+	std::string fname;
+	std::string srch;
+	std::string srch_opt;
+
+	switch (arg_c) { //Here we parse the additional arguments given to the function
+		case(3): {
+			 fname = va_arg(argl, std::string);
+			 srch = va_arg(argl, std::string);
+			 break;
+		}
+		case(4): {
+			fname = va_arg(argl, std::string);
+			srch = va_arg(argl, std::string);
+			srch_opt = va_arg(argl, std::string);
+			break;
+		}
+	}
+	va_end(argl);
+
+
+
+	
+	int opt_case = 0;
+
+	if (srch_opt == "-ol") {
+		opt_case = 1;
+	}
+	else if (srch_opt == "-oo") {
+		opt_case = 2;
+	}
+	else if (srch_opt == "-olo") {
+		opt_case = 3;
+	}
+	
+
+	
 	std::ifstream filein;
 	std::string line;
 	std::size_t found;
+	unsigned int lcounter = 0, fcounter = 0;
 	bool in_text = false;
-	filein.open(fname);
+
+	try {
+		
+		filein.open(fname);
+		std::string exception = "File cannot be opened";
+		if (!filein.good()) {
+			throw exception;
+		}
+	}
+	catch (std::string exception) {
+		std::cout << "File cannot be opened";
+	}
 
 	if (filein.good()) {
 		//std::cout << "File " << fname << " is open.";
 		while (std::getline(filein, line)) {
 
 			found = line.find(srch);
+			if (opt_case == 1 || opt_case == 3) {
+				lcounter++;
+			}
+		
 
 			if (found != std::string::npos) {
-				std::cout << line << '\n';
+
+				if (opt_case == 2 || opt_case == 3) {
+					fcounter++;
+				}
+
+				if (lcounter == 0) {
+					std::cout << line <<'\n';
+				}
+				else {
+					std::cout << lcounter << ':' << line << '\n';
+				}
 				in_text = true;
 			}
+			
 		}
+
 		if (in_text == false) {
 			std::cout << '"' << srch << '"' << " not found in " << fname;
 		}
+		if (opt_case == 2 || opt_case == 3 && in_text == true) {
+			std::cout << "Count of occurences: " << fcounter;
+		}
 		
 	}
-	else {
-		std::cout << "File " << fname << " cannot be opened.";
-	}
+	
+	
 	filein.close();
 
 	
